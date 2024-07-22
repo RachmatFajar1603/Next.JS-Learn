4182e6ee7bef12c84814ddf2c05a9e68864d342b
Rachmat Fajar <rachmat3@mhs.usk.ac.id>
2024-07-23 00:02:10 +0700

    update: pages and layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,3 @@
 export default function AboutPage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">About Page</h1>
-      <p className="text-lg">This is the about page</p>
-    </main>
-  );
+  return <h1>about page</h1>;
 }
