3699571dfbeb0c91967380960c543701627aa2d8
Rachmat Fajar <rachmat3@mhs.usk.ac.id>
2024-07-26 23:49:26 +0700

    update: intercepting routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,9 @@
+import { tree } from "next/dist/build/templates/app-page";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const isLogin = false;
+  const isLogin = true;
   if (!isLogin) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
